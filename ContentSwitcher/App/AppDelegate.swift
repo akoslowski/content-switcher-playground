9eeb86f18d41a7f6e58fe34e36e123b8ef3d2d26
akoslowski <2005824+akoslowski@users.noreply.github.com>
2025-08-05 12:30:19 +0200

    Generate Info.plist

--- ContentSwitcher/App/AppDelegate.swift
+++ ContentSwitcher/App/AppDelegate.swift
@@ -14,6 +14,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         configurationForConnecting connectingSceneSession: UISceneSession,
         options: UIScene.ConnectionOptions
     ) -> UISceneConfiguration {
-        UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+        let config = UISceneConfiguration(
+            name: "Default Configuration",
+            sessionRole: connectingSceneSession.role
+        )
+        config.delegateClass = SceneDelegate.self
+        return config
     }
 }
